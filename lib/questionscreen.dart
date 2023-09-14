a78dabf3c2de9353bb9a1fa9234ba424f837a80a
Morvalbrock <agilansr222@gmail.com>
2023-09-14 19:46:07 +0530

    create a question & button dynamicaly

--- lib/questionscreen.dart
+++ lib/questionscreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quizappproject2/answer_button.dart';
+import 'package:quizappproject2/models/data/questions.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -10,6 +12,38 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final currentQuestions = questions[0];
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestions.text,
+            style: const TextStyle(color: Colors.white),
+          ),
+          const SizedBox(
+            height: 30.0,
+          ),
+          ...currentQuestions.answers.map(
+            (answers) {
+              return AnswerButton(answerText: answers, onTap: () {});
+            },
+          ),
+          // AnswerButton(
+          //   answerText: currentQuestions.answers[1],
+          //   onTap: () {},
+          // ),
+          // AnswerButton(
+          //   answerText: currentQuestions.answers[2],
+          //   onTap: () {},
+          // ),
+          // AnswerButton(
+          //   answerText: currentQuestions.answers[3],
+          //   onTap: () {},
+          // ),
+        ],
+      ),
+    );
   }
 }
